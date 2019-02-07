ab0a70fac8587179ff87d5ba63ddc1707294b207
nikolay_semyonov <nikolay.semyonov@exponea.com>
2019-02-08 01:33:35 +0300

    k8s stuff added

--- worker/index.js
+++ worker/index.js
@@ -9,13 +9,13 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-    console.log('fib')
     if (index < 2) return  1;
     return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
     console.log('subscribe')
+    console.log(fib(parseInt(message)));
     redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
